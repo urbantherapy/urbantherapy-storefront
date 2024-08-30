9e98dfe40d57f8f79b45c6708513a0a0dfbbc075
Aymen <aymen@qq.com>
2024-08-30 20:51:21 +0200

    updates empty cart page

--- src/modules/cart/components/empty-cart-message/index.tsx
+++ src/modules/cart/components/empty-cart-message/index.tsx
@@ -1,25 +1,23 @@
-import { Heading, Text } from "@medusajs/ui"
-
 import InteractiveLink from "@modules/common/components/interactive-link"
 
 const EmptyCartMessage = () => {
   return (
     <div
-      className="py-48 px-2 flex flex-col justify-center items-start text-primary"
+      className="h-[85vh] py-32 md:py-48 px-2 flex flex-col justify-center items-start text-primary"
       data-testid="empty-cart-message"
     >
-      <Heading
-        level="h1"
-        className="flex flex-row text-5xl font-thin tracking-tight gap-x-2 items-baseline"
-      >
-        Shopping cart
-      </Heading>
-      <Text className="text-lg font-thin my-6 max-w-2xl">
+      <h1 className="flex flex-row text-3xl md:text-5xl font-thin tracking-tight gap-x-2 items-baseline">
+        Oops...
+      </h1>
+      <p className="text-base md:text-lg font-thin mt-4 mb-4 max-w-2xl text-sage-6">
         It looks like you haven&apos;t added anything to your cart yet. Take a
         look at our curated collections and find something special that speaks
-        to you. Each of our products is carefully selected to embody
-        sustainability, innovation, and craftsmanship.
-      </Text>
+        to you.
+      </p>
+      <p className="text-base md:text-lg font-thin mb-6 max-w-2xl text-sage-6">
+        Each of our products is carefully selected to embody sustainability,
+        innovation, and craftsmanship.
+      </p>
       <div>
         <InteractiveLink href="/store">Explore products</InteractiveLink>
       </div>
