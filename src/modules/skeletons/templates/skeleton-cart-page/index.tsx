cb604500a0fddcf43ac4fcdc84372a5f492db295
Aymen <aymen@qq.com>
2024-08-30 20:42:57 +0200

    updates skeleton cart components with new design

--- src/modules/skeletons/templates/skeleton-cart-page/index.tsx
+++ src/modules/skeletons/templates/skeleton-cart-page/index.tsx
@@ -7,39 +7,39 @@ import SkeletonOrderSummary from "@modules/skeletons/components/skeleton-order-s
 
 const SkeletonCartPage = () => {
   return (
-    <div className="py-12">
+    <div className="pt-48 py-12">
       <div className="content-container">
         <div className="grid grid-cols-1 small:grid-cols-[1fr_360px] gap-x-40">
-          <div className="flex flex-col bg-white p-6 gap-y-6">
-            <div className="bg-white flex items-start justify-between">
+          <div className="flex flex-col bg-sage-1 p-6 gap-y-6">
+            <div className="bg-sage-1 flex items-start justify-between">
               <div className="flex flex-col gap-y-2">
-                <div className="w-60 h-8 bg-gray-200 animate-pulse" />
-                <div className="w-48 h-6 bg-gray-200 animate-pulse" />
+                <div className="w-60 h-8 bg-sage-2 animate-pulse" />
+                <div className="w-48 h-6 bg-sage-2 animate-pulse" />
               </div>
               <div>
-                <div className="w-14 h-8 bg-gray-200 animate-pulse" />
+                <div className="w-14 h-8 bg-sage-2 animate-pulse" />
               </div>
             </div>
             <div>
               <div className="pb-3 flex items-center">
-                <div className="w-20 h-12 bg-gray-200 animate-pulse" />
+                <div className="w-20 h-12 bg-sage-2 animate-pulse" />
               </div>
               <Table>
-                <Table.Header className="border-t-0">
+                <Table.Header className="border-t-0 border-sage-2">
                   <Table.Row>
-                    <Table.HeaderCell className="!pl-0">
-                      <div className="w-10 h-6 bg-gray-200 animate-pulse" />
+                    <Table.HeaderCell className="!pl-0 bg-sage-1">
+                      <div className="w-10 h-6 bg-sage-2 animate-pulse" />
                     </Table.HeaderCell>
-                    <Table.HeaderCell></Table.HeaderCell>
-                    <Table.HeaderCell>
-                      <div className="w-16 h-6 bg-gray-200 animate-pulse" />
+                    <Table.HeaderCell className="bg-sage-1"></Table.HeaderCell>
+                    <Table.HeaderCell className="bg-sage-1">
+                      <div className="w-16 h-6 bg-sage-2 animate-pulse" />
                     </Table.HeaderCell>
-                    <Table.HeaderCell>
-                      <div className="w-12 h-6 bg-gray-200 animate-pulse" />
+                    <Table.HeaderCell className="bg-sage-1">
+                      <div className="w-12 h-6 bg-sage-2 animate-pulse" />
                     </Table.HeaderCell>
-                    <Table.HeaderCell className="!pr-0">
+                    <Table.HeaderCell className="!pr-0 bg-sage-1">
                       <div className="flex justify-end">
-                        <div className="w-12 h-6 bg-gray-200 animate-pulse" />
+                        <div className="w-12 h-6 bg-sage-2 animate-pulse" />
                       </div>
                     </Table.HeaderCell>
                   </Table.Row>
