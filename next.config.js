12d764fe451d35ce9e554dee55db48e826009af2
Aymen <aymen@qq.com>
2024-10-03 17:21:10 +0700

    adds urban-therapy-medusa S3 bucket to allowed image domains

--- next.config.js
+++ next.config.js
@@ -30,6 +30,10 @@ const nextConfig = withStoreConfig({
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "urban-therapy-medusa.s3.ap-southeast-2.amazonaws.com",
+      },
     ],
   },
 })
